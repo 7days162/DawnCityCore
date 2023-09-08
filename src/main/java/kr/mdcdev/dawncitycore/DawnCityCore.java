745ad75cb62118884fa4979a01ec3b30dc672589
7days162 <7days162@naver.com>
2023-09-08 11:51:24 +0000

    First

---
+++ src/main/java/kr/mdcdev/dawncitycore/DawnCityCore.java
@@ -0,0 +1,21 @@
+package kr.mdcdev.dawncitycore;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class DawnCityCore extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+        
+        // Plugin shutdown logic
+    }
+}
