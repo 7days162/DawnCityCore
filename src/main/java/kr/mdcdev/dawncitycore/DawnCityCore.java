7b42bccca62ce3a73d41d021457825aed5d24679
KyungMin Kim <7days162@naver.com>
2023-09-11 04:41:39 +0000

    Area Interface Constructor 1 (Test)

--- src/main/java/kr/mdcdev/dawncitycore/DawnCityCore.java
+++ src/main/java/kr/mdcdev/dawncitycore/DawnCityCore.java
@@ -6,6 +6,7 @@ public final class DawnCityCore extends JavaPlugin {
     
     @Override
     public void onEnable() {
+        System.out.println("아아 테스트 0911 사지");
     }
 
     @Override
--- src/main/java/kr/mdcdev/dawncitycore/container/Area.java
+++ src/main/java/kr/mdcdev/dawncitycore/container/Area.java
@@ -1,5 +1,7 @@
 package kr.mdcdev.dawncitycore.container;
 
+import org.bukkit.Location;
+
 public interface Area {
-    
+    boolean contains(Location loc);
 }
